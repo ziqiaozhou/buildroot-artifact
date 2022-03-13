f6336aeb1f53affcfdda16957e2bb135c9d967a4
Ziqiao Zhou <ziqiaozhou@microsoft.com>
2022-03-13 00:36:05 -0800

    remove python3

--- package/tss_msr/tss_msr.mk
+++ package/tss_msr/tss_msr.mk
@@ -3,7 +3,7 @@ TSS_MSR_SITE = $(call github,microsoft,TSS.MSR,$(TSS_MSR_VERSION))
 
 TSS_MSR_INSTALL_STAGING = YES
 define TSS_MSR_INSTALL_PY_LIB
-	cd $(@D) && $(INSTALL) TSS.Py/src/* -D -m 0755 $(TARGET_DIR)/usr/lib/python3.10/tss_msr/
+	cd $(@D) && $(INSTALL) TSS.Py/src/* -D -m 0755 $(TARGET_DIR)/usr/lib/python3.10/tssmsr/
 endef
 
 TSS_MSR_POST_INSTALL_TARGET_HOOKS += TSS_MSR_INSTALL_PY_LIB
